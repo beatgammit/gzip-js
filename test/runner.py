2312c03369c9b48d992f9b4b583548aee4e007f5
overkill <22098433+0verk1ll@users.noreply.github.com>
2019-07-15 15:07:29 -0400

    Improve PyLint Compliance

--- test/runner.py
+++ test/runner.py
@@ -8,49 +8,49 @@ import argparse
 import zipTest
 import unzipTest
 
-parser = argparse.ArgumentParser(description='Process command-line arguments')
-parser.add_argument('--file', '-f', metavar='path/to/file', type=str, nargs='?', help='Path to file to use for test')
-parser.add_argument('--level', '-l', metavar='#', type=int, nargs='?', help='Compression level')
-parser.add_argument('--no-delete', const=True, default=False, nargs='?', help='Don\'t delete files produced for test')
-parser.add_argument('--test', default='both', nargs='?', help='Which test to run (zip, unzip, both)')
+PARSER = argparse.ArgumentParser(description='Process command-line arguments')
+PARSER.add_argument('--file', '-f', metavar='path/to/file', type=str, nargs='?', help='Path to file to use for test')
+PARSER.add_argument('--level', '-l', metavar='#', type=int, nargs='?', help='Compression level')
+PARSER.add_argument('--no-delete', const=True, default=False, nargs='?', help='Don\'t delete files produced for test')
+PARSER.add_argument('--test', default='both', nargs='?', help='Which test to run (zip, unzip, both)')
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-allPassed = True
+ALL_PASSED = True
 
-outDir = 'test-outs'
+OUT_DIR = 'test-outs'
 
 # make the test-outs directory
 try:
-    os.mkdir(outDir)
+    os.mkdir(OUT_DIR)
 except:
     pass
 
-delete = not getattr(args, 'no_delete')
-level = getattr(args, 'level')
-inFile = getattr(args, 'file')
-test = getattr(args, 'test')
+DELETE = not getattr(ARGS, 'no_delete')
+LEVEL = getattr(ARGS, 'level')
+IN_FILE = getattr(ARGS, 'file')
+TEST = getattr(ARGS, 'test')
 
-if test == 'zip' or test == 'both':
+if TEST == 'zip' or TEST == 'both':
     print (Fore.CYAN + 'Running zip tests' + Fore.RESET)
     # if the user specifies a file, only run that test
-    if inFile != None:
-        allPassed = zipTest.runTest(inFile, level)
+    if IN_FILE is not None:
+        ALL_PASSED = zipTest.runTest(IN_FILE, LEVEL)
     else:
-        allPassed = zipTest.runAll(level)
+        ALL_PASSED = zipTest.runAll(LEVEL)
 
-if test == 'unzip' or test == 'both':
+if TEST == 'unzip' or TEST == 'both':
     print (Fore.CYAN + 'Running unzip tests' + Fore.RESET)
     # if the user specifies a file, only run that test
-    if inFile != None:
-        allPassed = unzipTest.runTest(inFile, level)
+    if IN_FILE is not None:
+        ALL_PASSED = unzipTest.runTest(IN_FILE, LEVEL)
     else:
-        allPassed = unzipTest.runAll(level)
+        ALL_PASSED = unzipTest.runAll(LEVEL)
 
-if delete:
-    shutil.rmtree(outDir)
+if DELETE:
+    shutil.rmtree(OUT_DIR)
 
-if allPassed:
+if ALL_PASSED:
     print (Fore.GREEN + 'All tests passed!' + Fore.RESET)
 else:
     print (Fore.RED + 'Automated test failed' + Fore.RESET)
